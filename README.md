acf420f7043c0e4b5dadbb6896f593ac7324c53a
Katusha-L <klubinina@yandex.ru>
2025-05-14 13:21:34 +0300

    new

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Проектная работа Mesto
 
+https://github.com/Katusha-L/mesto-project-ff.git
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+function createCardElement(cardData, buttomDelete) {
+    const cards = document.querySelector('#card-template');
+    const cardElement = cards.content.cloneNode(true).querySelector('.card');
 
-// @todo: Функция удаления карточки
+    const cardName = cardElement.querySelector('.card__title');
+    const cardImage = cardElement.querySelector('.card__image');
+   
+    cardImage.src = cardData.link;
+    cardName.textContent = cardData.name;
+    
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener ('click',() => 
+        {
+            buttomDelete(cardData, cardElement);  
+        });
+  return cardElement;
+}
+const placesList = document.querySelector('.places__list');
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(cardData => {
+    const cardElement = createCardElement(cardData, removeCard);
+    placesList.appendChild(cardElement);
+  });
+
+function removeCard(cardData, cardElement) {
+    cardElement.remove();
+  }
+
